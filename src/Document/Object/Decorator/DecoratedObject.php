ed63ca78c34b48daa031a031154efb05893e2ede
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-02-10 20:09:54 +0100

    Cleanup getTypeNameValue from decoratedObjects

--- src/Document/Object/Decorator/DecoratedObject.php
+++ src/Document/Object/Decorator/DecoratedObject.php
@@ -3,10 +3,8 @@
 namespace PrinsFrank\PdfParser\Document\Object\Decorator;
 
 use PrinsFrank\PdfParser\Document\Dictionary\Dictionary;
-use PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\Name\TypeNameValue;
 use PrinsFrank\PdfParser\Document\Document;
 use PrinsFrank\PdfParser\Document\Object\Item\ObjectItem;
-use PrinsFrank\PdfParser\Exception\InvalidArgumentException;
 use PrinsFrank\PdfParser\Exception\PdfParserException;
 
 abstract class DecoratedObject {
@@ -14,18 +12,10 @@ abstract class DecoratedObject {
         public readonly ObjectItem $objectItem,
         protected readonly Document $document
     ) {
-        $typeNameValue = $this->objectItem->getDictionary($document->stream)->getType();
-        if ($typeNameValue !== null && $typeNameValue !== $this->getTypeName()) {
-            throw new InvalidArgumentException(
-                sprintf('Expected object item of type %s, got %s', $this->getTypeName()->name ?? 'UNKNOWN', $typeNameValue->name ?? 'UNKNOWN')
-            );
-        }
     }
 
     /** @throws PdfParserException */
     public function getDictionary(): Dictionary {
         return $this->objectItem->getDictionary($this->document->stream);
     }
-
-    abstract protected function getTypeName(): ?TypeNameValue;
 }
