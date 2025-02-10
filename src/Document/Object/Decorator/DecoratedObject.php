0fedfa1d4cad5f739a20a11a8ba1da2f285bfe36
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-02-10 20:21:12 +0100

    Still check for correct decorator in DecoratedObject constructor

--- src/Document/Object/Decorator/DecoratedObject.php
+++ src/Document/Object/Decorator/DecoratedObject.php
@@ -5,6 +5,7 @@ namespace PrinsFrank\PdfParser\Document\Object\Decorator;
 use PrinsFrank\PdfParser\Document\Dictionary\Dictionary;
 use PrinsFrank\PdfParser\Document\Document;
 use PrinsFrank\PdfParser\Document\Object\Item\ObjectItem;
+use PrinsFrank\PdfParser\Exception\InvalidArgumentException;
 use PrinsFrank\PdfParser\Exception\PdfParserException;
 
 abstract class DecoratedObject {
@@ -12,6 +13,12 @@ abstract class DecoratedObject {
         public readonly ObjectItem $objectItem,
         protected readonly Document $document
     ) {
+        $typeNameValue = $this->objectItem->getDictionary($document->stream)->getType();
+        if ($typeNameValue !== null && $typeNameValue->getDecoratorFQN() !== static::class) {
+            throw new InvalidArgumentException(
+                sprintf('Object should have decorator %s, got %s', $typeNameValue->getDecoratorFQN(), static::class)
+            );
+        }
     }
 
     /** @throws PdfParserException */
