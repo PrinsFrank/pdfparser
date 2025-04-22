8e0077f19427d17b512d6375f8e30cf0ca726c5c
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-04-22 21:29:06 +0200

    Add text matrix

---
+++ src/Document/Text/Positioning/TextMatrix.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace PrinsFrank\PdfParser\Document\Text\Positioning;
+
+class TextMatrix {
+    public function __construct(
+        public readonly float $scaleX,  // a
+        public readonly float $shearX,  // b
+        public readonly float $shearY,  // c
+        public readonly float $scaleY,  // d
+        public readonly float $offsetX, // e
+        public readonly float $offsetY, // f
+    ) {
+    }
+}
