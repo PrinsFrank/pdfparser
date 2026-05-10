4928abccb664ab3c3f8a5e9b6ee70e349957facd
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2026-05-10 12:57:49 +0200

    Add test for retrieval of type/subtype from subdictionary

---
+++ tests/Unit/Document/Dictionary/DictionaryTest.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace PrinsFrank\PdfParser\Tests\Unit\Document\Dictionary;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use PrinsFrank\PdfParser\Document\Dictionary\Dictionary;
+use PrinsFrank\PdfParser\Document\Dictionary\DictionaryEntry\DictionaryEntry;
+use PrinsFrank\PdfParser\Document\Dictionary\DictionaryKey\DictionaryKey;
+use PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\Name\SubtypeNameValue;
+use PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\Name\TypeNameValue;
+
+#[CoversClass(Dictionary::class)]
+class DictionaryTest extends TestCase {
+    public function testGetType(): void {
+        static::assertSame(
+            TypeNameValue::FILE_SPEC,
+            (new Dictionary(new DictionaryEntry(DictionaryKey::TYPE, TypeNameValue::FILE_SPEC)))
+                ->getType(),
+        );
+    }
+
+    public function testGetTypeReturnsTypeFromSubDictionary(): void {
+        static::assertSame(
+            TypeNameValue::FILE_SPEC,
+            (new Dictionary(new DictionaryEntry(DictionaryKey::TYPE, new Dictionary(new DictionaryEntry(DictionaryKey::TYPE, TypeNameValue::FILE_SPEC)))))
+                ->getType(),
+        );
+    }
+
+    public function testGetSubType(): void {
+        static::assertSame(
+            SubTypeNameValue::FORM,
+            (new Dictionary(new DictionaryEntry(DictionaryKey::SUBTYPE, SubtypeNameValue::FORM)))
+                ->getSubType(),
+        );
+    }
+
+    public function testGetSubTypeReturnsSubTypeFromSubDictionary(): void {
+        static::assertSame(
+            SubTypeNameValue::FORM,
+            (new Dictionary(new DictionaryEntry(DictionaryKey::SUBTYPE, new Dictionary(new DictionaryEntry(DictionaryKey::SUBTYPE, SubtypeNameValue::FORM)))))
+                ->getSubType(),
+        );
+    }
+}
