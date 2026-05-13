05181df938d4af3192cee9bdf42d3a850eed110b
Vitor Mattos <vitor@php.rio>
2026-05-13 15:51:22 -0300

    feat(rectangle): add width and height helpers with coverage (#317)
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/Unit/Document/Dictionary/DictionaryValue/Rectangle/RectangleTest.php
+++ tests/Unit/Document/Dictionary/DictionaryValue/Rectangle/RectangleTest.php
@@ -3,11 +3,25 @@
 namespace PrinsFrank\PdfParser\Tests\Unit\Document\Dictionary\DictionaryValue\Rectangle;
 
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\Rectangle\Rectangle;
 
 #[CoversClass(Rectangle::class)]
 class RectangleTest extends TestCase {
+    /** @return iterable<string, array{0: Rectangle, 1: float, 2: float}> */
+    public static function provideRectanglesWithExpectedDimensions(): iterable {
+        yield 'normal coordinates' => [new Rectangle(42.22, 43.33, 44.44, 45.55), 2.22, 2.22];
+        yield 'inverted coordinates' => [new Rectangle(44.44, 45.55, 42.22, 43.33), 2.22, 2.22];
+        yield 'zero width and height' => [new Rectangle(10.0, 20.0, 10.0, 20.0), 0.0, 0.0];
+    }
+
+    #[DataProvider('provideRectanglesWithExpectedDimensions')]
+    public function testGetWidthAndHeight(Rectangle $rectangle, float $expectedWidth, float $expectedHeight): void {
+        static::assertEqualsWithDelta($expectedWidth, $rectangle->getWidth(), 0.00001);
+        static::assertEqualsWithDelta($expectedHeight, $rectangle->getHeight(), 0.00001);
+    }
+
     public function testFromValue(): void {
         static::assertNull(Rectangle::fromValue(''));
         static::assertNull(Rectangle::fromValue('[]'));
