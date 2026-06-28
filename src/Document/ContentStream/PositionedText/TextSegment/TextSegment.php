8df6e4a39696c77490e806bc5caf39e8f071788d
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2026-06-28 20:43:22 +0200

    Reuse existing logic to decode binary data from textStringValue

--- src/Document/ContentStream/PositionedText/TextSegment/TextSegment.php
+++ src/Document/ContentStream/PositionedText/TextSegment/TextSegment.php
@@ -4,7 +4,6 @@ namespace PrinsFrank\PdfParser\Document\ContentStream\PositionedText\TextSegment
 
 use PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\Name\EncodingNameValue;
 use PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\TextString\TextStringValue;
-use PrinsFrank\PdfParser\Document\Generic\Character\LiteralStringEscapeCharacter;
 use PrinsFrank\PdfParser\Document\Object\Decorator\Font;
 use PrinsFrank\PdfParser\Exception\ParseFailureException;
 
@@ -17,7 +16,7 @@ readonly class TextSegment {
     public function getText(Font $font): string {
         $text = '';
         if (str_starts_with($this->textString->textStringValue, '(') && str_ends_with($this->textString->textStringValue, ')')) {
-            $unescapedChars = LiteralStringEscapeCharacter::unescapeCharacters(substr($this->textString->textStringValue, 1, -1));
+            $unescapedChars = $this->textString->getBinaryString();
             if (preg_match('/^\\\\\d{3}$/', substr($this->textString->textStringValue, 1, -1)) === 1 && ($glyph = $font->getDifferences()?->getGlyph((int) octdec(substr($this->textString->textStringValue, 2, -1)))) !== null) {
                 $chars = $glyph->getChar();
             } elseif (strlen($unescapedChars) === 1 && ($glyph = $font->getDifferences()?->getGlyph(ord($unescapedChars))) !== null) {
